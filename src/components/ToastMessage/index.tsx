a62d943082845f0fe8e9264e779eba0a86154778
swttpch <igor.souza@ecto.digital>
2024-10-01 21:18:40 -0300

    refactor: optimize ToastMessage component with onClose functionality and close on duration ends

--- src/components/ToastMessage/index.tsx
+++ src/components/ToastMessage/index.tsx
@@ -1,19 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { IToastMessage } from '@/types/toast-message.d';
 
 import styles from './style.module.css';
 
 type ToastMessageProps = {
-	content: IToastMessage;
+  content: IToastMessage;
+  onClose: (id: string) => void;
 };
 
-export const ToastMessage: React.FC<ToastMessageProps> = ({ content: data }) => {
-	return (
-		<div className={styles.container} data-toast-type={data.type} data-toast-id={data.id}>
-			<span data-content>{data.message}</span>
+export const ToastMessage: React.FC<ToastMessageProps> = ({ content: data, onClose }) => {
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (data.duration) {
+      timer = setTimeout(() => {
+        onClose(data.id);
+      }, data.duration);
+    }
 
-			<span data-close>╳</span>
-		</div>
-	);
+    return () => timer && clearTimeout(timer);
+  }, []);
+  return (
+    <div className={styles.container} data-toast-type={data.type} data-toast-id={data.id}>
+      <span data-content>{data.message}</span>
+
+      <span data-close onClick={() => onClose(data.id)}>
+        ╳
+      </span>
+    </div>
+  );
 };
