335859d29c63bc41cc7c7a2bf70cdcfe283ed34e
swttpch <igor.souza@ecto.digital>
2024-10-01 21:41:41 -0300

    refactor: optimize getCities function and add type annotations

--- src/pages/api/cities/[length].ts
+++ src/pages/api/cities/[length].ts
@@ -5,20 +5,24 @@ import { ApiMethod } from '@/decorators/method';
 import { ICity } from '@/types/city';
 
 function loop(length: number) {
-	return Array.from({ length }, () => 1).map((_, index) => index + 1);
+  return Array.from({ length }, () => 1).map((_, index) => index + 1);
 }
 
 export default ApiMethod('GET')(async (req: NextApiRequest, res: NextApiResponse) => {
-	const length = Number(req.query.length);
+  const length = Number(req.query.length);
 
-	const cities: ICity[] = [];
-
-	for (const _ of loop(length)) {
-		cities.push({
-			id: faker.string.uuid(),
-			name: faker.location.city(),
-		});
-	}
-
-	return res.status(200).json(cities);
+  const cities = await getCities(length);
+  return res.status(200).json(cities);
 });
+
+export const getCities = async (length: number) => {
+  const cities: ICity[] = [];
+
+  for (const _ of loop(length)) {
+    cities.push({
+      id: faker.string.uuid(),
+      name: faker.location.city(),
+    });
+  }
+  return cities;
+};
